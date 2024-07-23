f4e7cefcd6a5caef2316c007a51785eac26f68c8
UNKNØWN <60184894+Unknxwn007@users.noreply.github.com>
2024-07-23 19:42:53 +0200

    Add files via upload

---
+++ moonwalk/MoonwalkingMod.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using RDR2;
+using RDR2.Math;
+using RDR2.Native;
+
+namespace moonwalk
+{
+    public class MoonwalkingMod : Script
+    {
+        public MoonwalkingMod()
+        {
+            Tick += OnTick;
+            Interval = 1;
+        }
+
+        private void OnTick(object sender, EventArgs evt)
+        {
+            if (!isPlayerWalking() && !isPlayerStopped()) { applyForceToPlayer(); }
+        }
+
+        private bool isPlayerWalking()
+        {
+            return Function.Call<bool>(Hash.IS_PED_RUNNING, Game.Player.Character.Handle);
+        }
+
+        private bool isPlayerStopped()
+        {
+            return Function.Call<bool>(Hash.IS_PED_STOPPED, Game.Player.Character.Handle);
+        }
+
+        private void applyForceToPlayer()
+        {
+            float forceMagnitude = 1.5f;
+            Vector3 forceDirection = Game.Player.Character.ForwardVector * forceMagnitude;
+
+            Function.Call(Hash.APPLY_FORCE_TO_ENTITY, Game.Player.Character.Handle, 1, forceDirection.X, forceDirection.Y, forceDirection.Z, 0.0f, 0.0f, 0.0f, 1, false, true, true, false, true);
+        }
+    }
+}
